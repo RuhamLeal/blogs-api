fe80d84e8ec0eee95c8b63a179e6b5173478c451
Ruham Leal <ruhamxlpro@hotmail.com>
2022-12-02 13:30:37 -0300

    feat: creating Category Model

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+const CategorySchema = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category', 
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'categories',
+    },
+  );
+  return Category;
+}
+
+module.exports = CategorySchema;
