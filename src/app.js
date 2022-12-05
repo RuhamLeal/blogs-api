1a21f958d807f4d28ad61b84f5637e73a8427c72
Ruham Leal <ruhamxlpro@hotmail.com>
2022-12-05 00:37:15 -0300

    refact: cleaning some useless code line

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const mainRouter = require('./routes/index');
 
-// ...
-
 const app = express();
 
 app.use(express.json());
 
 mainRouter(app);
 
-// ...
-
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
